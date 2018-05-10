1a407e554139b0adac6ec639e11e405d35de01c2
Lucas Marques de Paula <lucas.marques@objective.com.br>
2018-05-10 12:05:54 -0300

    Build service

---
+++ modules/guestbook/guestbook-service/src/main/java/br/com/objective/training/service/impl/EntryLocalServiceImpl.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package br.com.objective.training.service.impl;
+
+import br.com.objective.training.service.base.EntryLocalServiceBaseImpl;
+
+/**
+ * The implementation of the entry local service.
+ *
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link br.com.objective.training.service.EntryLocalService} interface.
+ *
+ * <p>
+ * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
+ * </p>
+ *
+ * @author Brian Wing Shun Chan
+ * @see EntryLocalServiceBaseImpl
+ * @see br.com.objective.training.service.EntryLocalServiceUtil
+ */
+public class EntryLocalServiceImpl extends EntryLocalServiceBaseImpl {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never reference this class directly. Always use {@link br.com.objective.training.service.EntryLocalServiceUtil} to access the entry local service.
+	 */
+}
