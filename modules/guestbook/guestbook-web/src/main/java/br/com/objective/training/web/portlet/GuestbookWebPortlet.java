478026fc5ed3e66c0bdd7c44e957bd19dd84b0f9
Lucas Marques de Paula <lucas.marques@objective.com.br>
2018-05-10 13:40:23 -0300

    Defining the component metadata properties

--- modules/guestbook/guestbook-web/src/main/java/br/com/objective/training/web/portlet/GuestbookWebPortlet.java
+++ modules/guestbook/guestbook-web/src/main/java/br/com/objective/training/web/portlet/GuestbookWebPortlet.java
@@ -28,14 +28,17 @@ import java.util.logging.Logger;
 @Component(
         immediate = true,
         property = {
-                "com.liferay.portlet.display-category=category.sample",
-                "com.liferay.portlet.instanceable=true",
-                "javax.portlet.display-name=guestbook-web Portlet",
+                "com.liferay.portlet.display-category=category.social",
+                "com.liferay.portlet.instanceable=false",
+                "com.liferay.portlet.scopeable=true",
+                "javax.portlet.display-name=Guestbook",
+                "javax.portlet.expiration-cache=0",
                 "javax.portlet.init-param.template-path=/",
                 "javax.portlet.init-param.view-template=/view.jsp",
                 "javax.portlet.name=" + GuestbookWebPortletKeys.GuestbookWeb,
                 "javax.portlet.resource-bundle=content.Language",
-                "javax.portlet.security-role-ref=power-user,user"
+                "javax.portlet.security-role-ref=power-user,user",
+                "javax.portlet.supports.mime-type=text/html"
         },
         service = Portlet.class
 )
