c34d4e44cc1e71f6f7198c7d688515f7c0f4cb35
Lucas Marques de Paula <lucas.marques@objective.com.br>
2018-05-11 16:25:03 -0300

    Fix portlet name

--- modules/guestbook/guestbook-web/src/main/java/br/com/objective/training/web/constants/GuestbookAdminPortletKeys.java
+++ modules/guestbook/guestbook-web/src/main/java/br/com/objective/training/web/constants/GuestbookAdminPortletKeys.java
@@ -5,7 +5,7 @@ package br.com.objective.training.web.constants;
  */
 public class GuestbookAdminPortletKeys {
 
-    public static final String GUESTBOOK_ADMIN = "Guestbook Admin";
+    public static final String GUESTBOOK_ADMIN = "br_com_objective_training_guestbook_portlet_GuestbookAdmin";
 
     public static final String MVC_PATH_EDIT = "/admin/edit.jsp";
     public static final String MVC_PATH_VIEW = "/admin/view.jsp";
