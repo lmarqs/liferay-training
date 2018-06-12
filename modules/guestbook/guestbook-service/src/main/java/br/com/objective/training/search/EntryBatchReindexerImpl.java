c4ce5d903ec9219adabb583dd389aa207c6efb54
Lucas Marques de Paula <lucas.marques@objective.com.br>
2018-06-12 17:55:46 -0300

    :building_construction: Implement ModelIndexerWriterContributor

---
+++ modules/guestbook/guestbook-service/src/main/java/br/com/objective/training/search/EntryBatchReindexerImpl.java
@@ -0,0 +1,51 @@
+package br.com.objective.training.search;
+
+
+import br.com.objective.training.model.Entry;
+import br.com.objective.training.service.EntryLocalService;
+import com.liferay.portal.kernel.dao.orm.Property;
+import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.search.batch.BatchIndexingActionable;
+import com.liferay.portal.search.indexer.IndexerDocumentBuilder;
+import com.liferay.portal.search.indexer.IndexerWriter;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+@Component(
+        immediate = true,
+        service = EntryBatchReindexer.class
+)
+public class EntryBatchReindexerImpl implements EntryBatchReindexer {
+
+    @Override
+    public void reindex(long guestbookId, long companyId) {
+        BatchIndexingActionable batchIndexingActionable = indexerWriter.getBatchIndexingActionable();
+
+        batchIndexingActionable.setAddCriteriaMethod(dynamicQuery -> {
+            Property guestbookIdPropery = PropertyFactoryUtil.forName("guestbookId");
+
+            dynamicQuery.add(guestbookIdPropery.eq(guestbookId));
+        });
+
+        batchIndexingActionable.setCompanyId(companyId);
+
+        batchIndexingActionable.setPerformActionMethod((Entry entry) -> {
+            Document document = indexerDocumentBuilder.getDocument(entry);
+
+            batchIndexingActionable.addDocuments(document);
+        });
+
+        batchIndexingActionable.performActions();
+    }
+
+    @Reference
+    protected EntryLocalService entryLocalService;
+
+    @Reference(target = "(indexer.class.name=br.com.objective.training.model.Entry)")
+    protected IndexerDocumentBuilder indexerDocumentBuilder;
+
+    @Reference(target = "(indexer.class.name=br.com.objective.training.model.Entry)")
+    protected IndexerWriter<Entry> indexerWriter;
+
+}
