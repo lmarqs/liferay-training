7ae01e916e25a9ec12aee0cf680aa6881218ce9c
Lucas Marques de Paula <lucas.marques@objective.com.br>
2018-07-16 15:47:44 -0300

    Remove author

--- modules/guestbook/guestbook-api/src/main/java/com/liferay/blade/samples/guestbook/constants/GuestbookAdminPanelCategoryKeys.java
+++ modules/guestbook/guestbook-api/src/main/java/com/liferay/blade/samples/guestbook/constants/GuestbookAdminPanelCategoryKeys.java
@@ -1,8 +1,5 @@
 package com.liferay.blade.samples.guestbook.constants;
 
-/**
- * @author lucas
- */
 public class GuestbookAdminPanelCategoryKeys {
 
     public static final String CONTROL_PANEL_CATEGORY = "com.liferay.blade.samples.guestbook.web.application.list.GuestbookAdminPanelCategory";
